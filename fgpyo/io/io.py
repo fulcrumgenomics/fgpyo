4c7506f0947633b9a86fda135be91c5f6225f591
samfulcrum <samantha@fulcrumgenomics.com>
2023-01-11 11:21:06 -0700

    Updated io.py docstring

--- fgpyo/io/io.py
+++ fgpyo/io/io.py
@@ -1,106 +1,23 @@
 """
 I/O
 -------
+# TODO fix examples section/desciptions
+Module for reading and writing
 
-Module for storing, reading, and writing metric-like tab-delimited information.
-
-Metric files are tab-delimited, contain a header, and zero or more rows for metric values.  This
-makes it easy for them to be read in languages like `R`.  For example, a row per person, with
-columns for age, gender, and address.
-
-The :class:`~fgpyo.util.metric.Metric` class makes it easy to read, write, and store one or metrics
-of the same type, all the while preserving types for each value in a metric.  It is an abstract
-base class decorated by `attr <https://www.attrs.org/en/stable/examples.html>`_, with attributes
-storing one or more typed values.
+The :class:`~fgpyo.io.io.IO` class makes it easy to...
 
 Examples
 ~~~~~~~~
 
-Defining a new metric class:
+Defining a new IO class:
 
 .. code-block:: python
 
-   >>> from fgpyo.util.metric import Metric
+   >>> from fgpyo.io.io import IO
    >>> import attr
    >>> @attr.s(auto_attribs=True, frozen=True)
-   ... class Person(Metric["Person"]):
-   ...     name: str
-   ...     age: int
-
-Getting the attributes for a metric class.  These will be used for the header when reading and
-writing metric files.
-
-.. code-block:: python
-
-   >>> Person.header()
-   ['name', 'age']
-
-Getting the values from a metric class instance.  The values are in the same order as the header.
-
-.. code-block:: python
-
-   >>> list(Person(name="Alice", age=47).values())
-   ["Alice", 47]
-
-Writing a list of metrics to a file:
-
-.. code-block:: python
-
-   >>> metrics = [
-   ...     Person(name="Alice", age=47),
-   ...     Person(name="Bob", age=24)
-   ... ]
-   >>> from pathlib import Path
-   >>> Person.write(Path("/path/to/metrics.txt"), *metrics)
-
-Then the contents of the written metrics file:
-
-.. code-block:: console
-
-   $ column -t /path/to/metrics.txt
-   name   age
-   Alice  47
-   Bob    24
-
-Reading the metrics file back in:
-
-.. code-block:: python
-
-   >>> list(Person.read(Path("/path/to/metrics.txt")))
-   [Person(name='Alice', age=47, address=None), Person(name='Bob', age=24, address='North Pole')]
-
-Formatting and parsing the values for custom types is supported by overriding the
-:func:`~fgpyo.util.metric.Metric._parsers` and
-:func:`~fgpyo.util.metric.Metric.format_value` methods.
-
-.. code-block:: python
-
-   >>> @attr.s(auto_attribs=True, frozen=True)
-   ... class Name:
-   ...     first: str
-   ...     last: str
-   ...     @classmethod
-   ...     def parse(cls, value: str) -> "Name":
-   ...          fields = value.split(" ")
-   ...          return Name(first=fields[0], last=fields[1])
-   >>> @attr.s(auto_attribs=True, frozen=True)
-   ... class Person(Metric["Person"]):
-   ...     name: Name
-   ...     age: int
-   ...     def _parsers(cls) -> Dict[type, Callable[[str], Any]]:
-   ...         return {Name: lambda value: Name.parse(value=value)}
-   ...     @classmethod
-   ...     def format_value(cls, value: Any) -> str:
-   ...         if isinstance(value, (Name)):
-   ...             return f"{value.first} {value.last}"
-   ...         else:
-   ...             return super().format_value(value=value)
-   >>> Person.parse(fields=["john doe", "42"])
-   Person(name=Name(first='john', last='doe'), age=42)
-   >>> Person(name=Name(first='john', last='doe'), age=42, address=None).formatted_values()
-   ["first last", "42"]
-
-"""
+   # TODO 
+   """
 
 import gzip
 import os

b88175d042e32b3a1f32512ab332d6f5dec0a9fd
Nils Homer <nilshomer@gmail.com>
2023-03-09 13:37:08 -0700

    fix test

--- fgpyo/util/tests/test_string.py
+++ fgpyo/util/tests/test_string.py
@@ -11,5 +11,5 @@ def test_columnit_unequal_number_of_columns() -> None:
 def test_columnit_ok() -> None:
     assert column_it(rows=[["1,1", "1,2"], ["2,1", "2,2"]]) == "1,1 1,2\n2,1 2,2"
     assert column_it(rows=[["1,1", "1,2"], ["2,1", "2,2"]], delimiter="|") == "1,1|1,2\n2,1|2,2"
-    assert column_it(rows=[["1,1", "1,2x"], ["2,1", "2,2"]]) == "1,1 1,2x\n2,1 2,2 "
+    assert column_it(rows=[["1,1", "1,2x"], ["2,1", "2,2"]]) == "1,1 1,2x\n2,1  2,2"
     assert column_it(rows=[["1,1x", "1,2"], ["2,1", "2,2"]]) == "1,1x 1,2\n 2,1 2,2"

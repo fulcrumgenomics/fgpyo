636c21dc8f873372f13557c5ac325c00cf43c6d5
Nils Homer <nilshomer@gmail.com>
2023-03-09 13:32:44 -0700

    fix test case

--- fgpyo/util/tests/test_string.py
+++ fgpyo/util/tests/test_string.py
@@ -12,4 +12,4 @@ def test_columnit_ok() -> None:
     assert column_it(rows=[["1,1", "1,2"], ["2,1", "2,2"]]) == "1,1 1,2\n2,1 2,2"
     assert column_it(rows=[["1,1", "1,2"], ["2,1", "2,2"]], delimiter="|") == "1,1|1,2\n2,1|2,2"
     assert column_it(rows=[["1,1", "1,2x"], ["2,1", "2,2"]]) == "1,1 1,2x\n2,1 2,2 "
-    assert column_it(rows=[["1,1x", "1,2"], ["2,1", "2,2"]]) == "1,1x 1,2\n2,1  2,2"
+    assert column_it(rows=[["1,1x", "1,2"], ["2,1", "2,2"]]) == "1,1x 1,2\n 2,1 2,2"

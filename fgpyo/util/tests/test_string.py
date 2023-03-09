504b2a885480eddefbe21467b243313cc400499c
Nils Homer <nilshomer@gmail.com>
2023-03-09 10:54:32 -0700

    fix

--- fgpyo/util/tests/test_string.py
+++ fgpyo/util/tests/test_string.py
@@ -1,8 +1,11 @@
+import pytest
+
 from fgpyo.util.string import column_it
 
 
 def test_columnit_unequal_number_of_columns() -> None:
-    column_it(rows=[["1,1", "1,2"], ["2,1"]])
+    with pytest.assert_raises(IndexError):
+        column_it(rows=[["1,1", "1,2"], ["2,1"]])
 
 
 def test_columnit_ok() -> None:

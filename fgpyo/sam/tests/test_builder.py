2baa8f0169e31c0b9e29a3893185b214e1e29a09
Matt Stone <matt@fulcrumgenomics.com>
2023-10-17 11:54:58 -0400

    fix: pysam version doesn't support is_mapped

--- fgpyo/sam/tests/test_builder.py
+++ fgpyo/sam/tests/test_builder.py
@@ -180,11 +180,11 @@ def test_chrom1_chrom2() -> None:
     assert r2.is_reverse
 
     r1, r2 = builder.add_pair(chrom1="chr1", start1=1000)
-    assert r1.is_mapped
+    assert not r1.is_unmapped
     assert r2.is_unmapped
 
     r1, r2 = builder.add_pair(chrom2="chr1", start2=1000)
-    assert r2.is_mapped
+    assert not r2.is_unmapped
     assert r1.is_unmapped
 
     with pytest.raises(ValueError, match="When using chrom1 or chrom2, both must be specified."):

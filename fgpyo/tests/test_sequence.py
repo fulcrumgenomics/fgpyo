0465a0a779044af392c00592570ba2c59d13bef7
Erin McAuley <erin@fulcrumgenomics.com>
2024-07-01 16:31:03 -0400

    feat: add longest_hp_len() to fgpyo/sequence.py

--- fgpyo/tests/test_sequence.py
+++ fgpyo/tests/test_sequence.py
@@ -2,6 +2,7 @@
 
 import pytest
 
+from fgpyo.sequence import longest_hp_length
 from fgpyo.sequence import reverse_complement
 
 
@@ -15,3 +16,12 @@ def test_reverse_complement() -> None:
 
     with pytest.raises(KeyError):
         reverse_complement("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+
+def test_homopolymer() -> None:
+    assert longest_hp_length("") == 0
+    assert longest_hp_length("A") == 1
+    assert longest_hp_length("AAAA") == 4
+    assert longest_hp_length("ACTACGATTTTTACGAT") == 5
+    assert longest_hp_length("ACTACGATTTTTACGAT") == 5
+    assert longest_hp_length("TTTTACTACGAACGAGTTTTT") == 5

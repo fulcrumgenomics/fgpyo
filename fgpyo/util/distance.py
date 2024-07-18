5863b3af38e9eed82a360960990ffb366a0be74b
znorgaard <zach@fulcrumgenomics.com>
2024-07-18 08:26:08 -0700

    docs: provide additional clarification

--- fgpyo/util/distance.py
+++ fgpyo/util/distance.py
@@ -35,8 +35,8 @@ def levenshtein(string1: str, string2: str) -> int:
     """
     N: int = len(string1)
     M: int = len(string2)
-    # Initialize N + 1 x M + 1 matrix with final row/column representing empty string
-    # Fill in initial values for empty string sub-problem comparisons
+    # Initialize N + 1 x M + 1 matrix with final row/column representing the empty string.
+    # Fill in initial values for empty string sub-problem comparisons.
     #   A D C "
     # A - - - 3
     # B - - - 2
@@ -47,7 +47,7 @@ def levenshtein(string1: str, string2: str) -> int:
         matrix[N][j] = M - j
     for i in range(N + 1):
         matrix[i][M] = N - i
-    # Fill in matrix from bottom up using previous solutions
+    # Fill in matrix from bottom up using previous sub-problem solutions.
     #   A D C "      A D C "      A D C "      A D C "      A D C "
     # A - - - 3    A - - - 3    A - - 2 3    A - 2 2 3    A 1 2 2 3
     # B - - - 2 -> B - - 1 2 -> B - 1 1 2 -> B 2 1 1 2 -> B 2 1 1 2
@@ -56,7 +56,7 @@ def levenshtein(string1: str, string2: str) -> int:
     for i in range(N - 1, -1, -1):
         for j in range(M - 1, -1, -1):
             if string1[i] == string2[j]:
-                matrix[i][j] = matrix[i + 1][j + 1]
+                matrix[i][j] = matrix[i + 1][j + 1] # No Operation
             else:
                 matrix[i][j] = 1 + min(
                     matrix[i + 1][j],  # Deletion

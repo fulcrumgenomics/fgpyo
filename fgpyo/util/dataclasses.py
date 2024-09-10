be9616cd1fc68433ac6638924e7e4fd3a6c88443
Matt Stone <matt@fulcrumgenomics.com>
2024-09-10 12:31:02 -0400

    fix: import from inspect

--- fgpyo/util/dataclasses.py
+++ fgpyo/util/dataclasses.py
@@ -1,23 +1,7 @@
-from dataclasses import Field
 from dataclasses import fields
 from typing import Any
-from typing import ClassVar
-from typing import Dict
-from typing import Protocol
 
-
-class DataclassInstance(Protocol):
-    """
-    Type hint for a non-specific instance of a dataclass.
-
-    `DataclassReader` is an iterator over instances of the specified dataclass type. However, the
-    actual type is not known prior to instantiation. This `Protocol` is used to type hint the return
-    signature of `DataclassReader`'s `__next__` method.
-
-    https://stackoverflow.com/a/55240861
-    """
-
-    __dataclass_fields__: ClassVar[Dict[str, Field]]
+from fgpyo.util.inspect import DataclassInstance
 
 
 class DataSubclassMixin:

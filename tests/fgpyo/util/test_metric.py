5060e80744570da4bd035f0e4fb82d23436b05ce
Nils Homer <nilshomer@gmail.com>
2025-01-28 12:04:11 -0700

    small test

--- tests/fgpyo/util/test_metric.py
+++ tests/fgpyo/util/test_metric.py
@@ -434,6 +434,15 @@ def test_metric_strips_trailing_whitespace(tmp_path: Path, data_and_classes: Dat
     assert persons[2].name == "Someone Else"
     assert persons[2].age == 47
 
+    persons = list(data_and_classes.Person.read(test_tsv, strip_whitespace=False))
+    assert len(persons) == 3
+    assert persons[0].name == " John Doe "
+    assert persons[0].age == 42
+    assert persons[1].name == "Jane Doe"
+    assert persons[1].age == 35
+    assert persons[2].name == " Someone Else "
+    assert persons[2].age == 47
+
 
 @pytest.mark.parametrize("data_and_classes", (attr_data_and_classes, dataclasses_data_and_classes))
 def test_metric_items(data_and_classes: DataBuilder) -> None:

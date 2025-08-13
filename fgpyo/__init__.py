9ef41612013058fd88bc0a000cb3c014afef40e2
Matt Stone <matt@fulcrumgenomics.com>
2025-08-13 15:35:27 -0400

    feat: require
    fix: Optional
    
    doc: reword

--- fgpyo/__init__.py
+++ fgpyo/__init__.py
@@ -1,7 +1,15 @@
 from importlib.metadata import PackageNotFoundError
 from importlib.metadata import version
 
+from fgpyo._requirements import RequirementError
+from fgpyo._requirements import require
+
 try:
     __version__ = version("fgpyo")
 except PackageNotFoundError:
     __version__ = "dev"  # package is not installed
+
+__all__ = [
+    "require",
+    "RequirementError",
+]

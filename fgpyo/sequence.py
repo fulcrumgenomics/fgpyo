2dcf4fb2c067d6681122408dac06024e5367f0b1
Ted Brookings <ted@fulcrumgenomics.com>
2025-11-04 16:27:34 -0500

    Speed up reverse_complement by using str.translate (#262)

--- fgpyo/sequence.py
+++ fgpyo/sequence.py
@@ -9,8 +9,10 @@ If you are performing many distance calculations, using a C based method is pref
 ex. https://pypi.org/project/Distance/
 """
 
+from types import MappingProxyType
 from typing import Dict
 from typing import List
+from typing import Optional
 
 _COMPLEMENTS: Dict[str, str] = {
     # Discrete bases
@@ -56,6 +58,19 @@ _COMPLEMENTS: Dict[str, str] = {
 }
 
 
+# Get str of all invalid nucleotide characters (strings missing from _COMPLEMENTS).
+_INVALID_BASES: str = "".join(c for c in (chr(o) for o in range(256)) if c not in _COMPLEMENTS)
+# Use str.maketrans to create a table matching ascii codes of bases to ascii codes of complements.
+_COMPLEMENTS_TABLE: MappingProxyType[int, Optional[int]] = MappingProxyType(
+    str.maketrans("".join(_COMPLEMENTS.keys()), "".join(_COMPLEMENTS.values()), _INVALID_BASES)
+)
+"""
+This table allows faster reverse-complement than directly using _COMPLEMENTS iterating over bases.
+Characters from _INVALID_BASES will be mapped to None and omitted from translation, which can be
+detected as an error by noting decreased string length.
+"""
+
+
 def complement(base: str) -> str:
     """Returns the complement of any base."""
     if len(base) != 1:
@@ -73,7 +88,13 @@ def reverse_complement(bases: str) -> str:
     Returns:
         the reverse complement of the provided base string
     """
-    return "".join([_COMPLEMENTS[b] for b in bases[::-1]])
+    rev_comp = bases.translate(_COMPLEMENTS_TABLE)[::-1]
+    if len(rev_comp) != len(bases):
+        # There were invalid characters that weren't translated.
+        # Raise KeyError with all the invalid bases.
+        bad_bases = "".join({base for base in bases if base not in _COMPLEMENTS})
+        raise KeyError(f"Invalid bases found: {bad_bases}")
+    return rev_comp
 
 
 def gc_content(bases: str) -> float:
